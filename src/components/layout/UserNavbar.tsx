8954d959f4387e3d3096180d8b302f3a7576d758
Marmo77 <matyskamela@gmail.com>
2025-08-18 00:46:18 +0200

    Share Profile button
    - Share Profile button -> copy link to github repository.

--- src/components/layout/UserNavbar.tsx
+++ src/components/layout/UserNavbar.tsx
@@ -1,7 +1,17 @@
-import React, { useState, type ReactNode } from "react";
-import { Share2, Settings, MoveLeft, Move, X, LogOut } from "lucide-react";
+import React, { useEffect, useState, type ReactNode } from "react";
+import {
+  Share2,
+  Settings,
+  MoveLeft,
+  Move,
+  X,
+  LogOut,
+  Terminal,
+} from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
 import { getUserFromStorage } from "../../storage/Users";
 import { useLocation } from "react-router-dom";
+import { Alert, AlertTitle, AlertDescription } from "../ui/alert";
 
 const iconMap = {
   arrow: <MoveLeft className="img-small text-muted-foreground" />,
@@ -24,8 +34,45 @@ function UserNavbar({
   const currentUser = getUserFromStorage();
   const phnm = useLocation().pathname;
 
+  // SHARE Profil
+  const [share, setShare] = useState<boolean>(false);
+  const gitRepo = "https://github.com/Marmo77/gym-worker-exercise";
+  const handleShare = () => {
+    setShare(!share);
+    navigator.clipboard.writeText(
+      "https://github.com/Marmo77/gym-worker-exercise"
+    );
+  };
+  useEffect(() => {
+    const timeout = setTimeout(() => setShare(false), 2000);
+    return () => clearTimeout(timeout);
+  }, [share]);
+
   return (
     <header className="bg-background/10 sticky top-0 z-50 shadow-lg backdrop-blur">
+      <AnimatePresence>
+        {share && (
+          <motion.div
+            className="flex justify-center items-center"
+            key={"alert"}
+            initial={{ opacity: 0, y: -30 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -30 }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+          >
+            <Alert
+              className="absolute bg-background shadow-2xs drop-shadow-lg max-w-96 top-5 "
+              variant={"destructive"}
+            >
+              <Terminal />
+              <AlertTitle>Great job!</AlertTitle>
+              <AlertDescription>
+                Its already in your clipboard!
+              </AlertDescription>
+            </Alert>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <div className="max-w-7xl mx-auto px-6 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center gap-6">
@@ -60,7 +107,10 @@ function UserNavbar({
           </div>
           <div className="flex items-center gap-4">
             {/* SHARE AND SETTINGS */}
-            <button className="flex gap-4 items-center text-sm px-2 py-1.5 rounded-lg shadow-md bg-accent/40 group hover:scale-105 duration-300 transition-all hover:bg-accent click-pressed">
+            <button
+              className="flex gap-4 items-center text-sm px-2 py-1.5 rounded-lg shadow-md bg-accent/40 group hover:scale-105 duration-300 transition-all hover:bg-accent click-pressed"
+              onClick={handleShare}
+            >
               <Share2 className="img-small text-accent-foreground group-hover:-rotate-4 group-hover:scale-[110%] duration-200 delay-75" />
               <span>Share profile</span>
             </button>
